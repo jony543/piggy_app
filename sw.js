f5a4b48cad28486ebb54cf63ac7c3e2f5bebe497
Rani Gera <ranigera.aristo@gmail.com>
2021-05-20 11:50:20 +0300

    fixing something in the service worker that became necessary following the previous implementation of resending missed_data to the server

--- sw.js
+++ sw.js
@@ -32,6 +32,7 @@ self.addEventListener('fetch', function (event) {
             return cache.match(event.request).then(function (response) {
                 return response || fetch(event.request).then(function (response) {
                     if (!event.request.url.includes('api/session/list?subId') // check that this is not the call to the data from the server (which I don't want to cache)
+                        && !event.request.url.includes('api/session?subId') // added by Rani to do the same as the one above; became necessary after the implementation of trying to resend the missed data
                         && !event.request.url.includes('ongoing_exclusion.js')) {
                         //&& !event.request.url.includes('json')) // to prevent the manifest from being cached // ** add this condition if using a common manifest.json method for all subjects (that uses a start_url) 
                         cache.put(event.request, response.clone());
